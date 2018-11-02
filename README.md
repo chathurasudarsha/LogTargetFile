d3910f0e2169431248bd68e34bbadbedbf26814e
chathura <chathura@wayforward.lk>
2018-11-02 22:50:12 +0530

    update readme and customizing message format

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # LogTargetFile
+
+Customize the log message
+Show and ordering message units
+
+
+Installation
+--
+
+Just run in your console:
+
+    composer require donsimon/yii2-log-target-file "*"
+
+or add 
+
+    "donsimon/yii2-log-target-file": "*"
+
+to the require section of your composer.json file.
+
+Usage
+--
+
+Add following code your configuration
+
+        'bootstrap' => ['log'],
+
+Then,
+ 
+Add following code configurations components
+        'log' => [
+        //          'traceLevel' => YII_DEBUG ? 3 : 0,
+                    'targets' => [
+                          [  
+                                'class' => 'donsimon\log\logTargetFile',  
+                                'levels' => ['info','warning','error'],
+                                'categories' => ['user','application'],
+                                'logVars' => ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_SERVER']
+                                'logFile' => '@backend/runtime/logs/appAndUser.log',
+                                'showLogMessageOrder' =>['timestamp','prefix','level','category','message']
+                                'prefixWithSession'=>true
+                            ]
+                    ],
+        ],
+
+After `Yii::info($text, 'user');` your log will update.
+
+What you can do here,
+
+You can change log file name using logFile.
+You can add many categories.
+You can add many levels (refer Yii2 logging)
+You can print vars using logVars.
+
+`showLogMessageOrder` is a array with full log message. You can customize and change the order of message units. 
+`prefixWithSession` can use for show, hide seesionId from prefix. 
+ 
+
--- src/logTargetFile.php
+++ src/logTargetFile.php
@@ -2,41 +2,73 @@
 
 /**
  * @copyright 2018 Don Simon P C S Ariyathilaka
+ * customize the log message
+ * show and ordering message units
  */
 namespace donsimon\log;
 
-
 use Yii; 
 use yii\web\Request;
+use yii\helpers\VarDumper;
+use yii\log\Logger;
 
-class LogTargetFile extends \yii\log\FileTarget
-{
-    /**
-     * @inheritdoc
-     */
+class LogTargetFile extends \yii\log\FileTarget{
+
     public $categories = [];
+    public $showLogMessageOrder =[];
     private $message;
-    
-    public function formatMessage($message)
-    {
+    public $prefixWithSession=false;
+
+    public function formatMessage($message){
         $this->message = $message;
-      // list($text, $level, $category, $timestamp) = $message;
-        if (isset($message[4])) {
-            $message[4] = [];
+        list($text, $level, $category, $timestamp) = $message;
+        $level = Logger::getLevelName($level);
+        if (!is_string($text)) {
+            // exceptions may not be serializable if in the call stack somewhere is a Closure
+            if ($text instanceof \Throwable || $text instanceof \Exception) {
+                $text = (string) $text;
+            } else {
+                $text = VarDumper::export($text);
+            }
         }
-        //$prefix = $this->getMessagePrefix($message);
-        return parent::formatMessage($message);
+        $traces = [];
+        if (isset($message[4])) {
+            foreach ($message[4] as $trace) {
+                $traces[] = "in {$trace['file']}:{$trace['line']}";
+            }
+        }
+
+        $prefix = $this->getMessagePrefix($message);
+        $messageAppend = '';
+        foreach($this->showLogMessageOrder as $showing){
+            if($showing=='timestamp'){
+               $messageAppend .= " ".$this->getTime($timestamp);
+            }
+            if($showing=='level'){
+               $messageAppend .= "[$level]";
+            }
+            if($showing=='category'){
+               $messageAppend .= "[$category]";
+            }
+            if($showing=='prefix'){
+               $messageAppend .= "{$prefix}";
+            }
+            if($showing=='message'){
+               $messageAppend .= " $text";
+            }
+            
+        }
+            return trim($messageAppend," ")
+                    . (empty($traces) ? '' : "\n    " . implode("\n    ", $traces));
     }
     
     public function getMessagePrefix($message){
         if ($this->prefix !== null) {
             return call_user_func($this->prefix, $message);
         }
-
         if (Yii::$app === null) {
             return '';
         }
-
         $request = Yii::$app->getRequest();
         $ip = $request instanceof Request ? $request->getUserIP() : '-';
 
@@ -47,9 +79,13 @@ class LogTargetFile extends \yii\log\FileTarget
         } else {
             $userID = '';
         }
+        /* @var $session \yii\web\Session */
+        $session = Yii::$app->has('session', true) ? Yii::$app->get('session') : null;
+        $sessionID = $session && $session->getIsActive() ? $session->getId() : '-';
+        
+        if($this->prefixWithSession && $sessionID!='-'){
+            return "[$ip][$userID][$sessionID]";
+        }
         return "[$ip][$userID]";
     }
-    
-
-    
 }
